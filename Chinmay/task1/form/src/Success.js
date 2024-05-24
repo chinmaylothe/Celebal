130f0e51b38670d6433cf5d063ba2ba5e6d6f9c9
Adi <98210910+adityas-github@users.noreply.github.com>
2024-05-24 09:40:58 +0530

    Update Success.js

--- Chinmay/task1/form/src/Success.js
+++ Chinmay/task1/form/src/Success.js
@@ -1,20 +1,20 @@
+
+// Success.js
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Success = () => {
   const location = useLocation();
-  const formData = location.state.formData;
+  const { formData } = location.state;
 
   return (
     <div>
-      <h1>Registration Successful!</h1>
-      <h2>Details:</h2>
+      <h1>Success</h1>
       <p>First Name: {formData.firstName}</p>
       <p>Last Name: {formData.lastName}</p>
       <p>Username: {formData.username}</p>
       <p>Email: {formData.email}</p>
-      <p>Password: {formData.password}</p>
-      <p>Phone Number: {formData.phone}</p>
+      <p>Phone: {formData.phone}</p>
       <p>Country: {formData.country}</p>
       <p>City: {formData.city}</p>
       <p>PAN Number: {formData.panNo}</p>
